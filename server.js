9523058b17ba20433a489ec79e982897a660472c
unknown <bcneves13@gmail.com>
2023-03-23 19:32:25 -0300

    example json to get

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("hello word")
+    res.json({
+        "menu": {
+            "id": "file",
+            "value": "File",
+            "popup": {
+                "menuitem": [
+                    { "value": "New", "onclick": "CreateNewDoc()" },
+                    { "value": "Open", "onclick": "OpenDoc()" },
+                    { "value": "Close", "onclick": "CloseDoc()" }
+                ]
+            }
+        }
+    })
 })
 
 app.listen(process.env.PORT, () => {
